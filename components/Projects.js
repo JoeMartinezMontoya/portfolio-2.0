30110eb9b981bcc7525f871de6c1c68b40712a66
Joë <martinezmontoyajoe@gmail.com>
2025-01-13 15:00:31 +0100

    feat(projects): added modal pop-up
    custom projects data

--- components/Projects.js
+++ components/Projects.js
@@ -1,20 +1,47 @@
-import { motion } from 'framer-motion';
+'use client';
+
+import { useState } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 import Data from '../projects.json';
-import { FaCode, FaYoutube, FaGamepad, FaEye } from 'react-icons/fa'; // Import des icônes
+import {
+  FaCode,
+  FaYoutube,
+  FaGamepad,
+  FaEye,
+  FaTimes,
+  FaPauseCircle,
+  FaPlayCircle,
+  FaCheckCircle,
+  FaArrowLeft,
+  FaArrowRight,
+} from 'react-icons/fa';
 
 export default function Projects() {
+  const [selectedProject, setSelectedProject] = useState(null);
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
   const getPatternByProjectName = (name) => {
     switch (name.toLowerCase()) {
       case 'petscare':
-        return "url('/patterns/petscare-pattern.png')";
-      case 'speye':
-        return "url('/patterns/speye-pattern.svg')";
+        return "url('/images/petscare-pattern.png')";
       case 'pixelpathfinder':
-        return "url('/patterns/pixelpathfinder-pattern.png')";
+        return "url('/images/pixelpathfinder-pattern.png')";
       case 'labyrinthe':
-        return "url('/patterns/labyrinthe-pattern.png')";
+        return "url('/images/labyrinthe-pattern.png')";
       case 'speye':
-        return "url('/patterns/speye-pattern.png')";
+        return "url('/images/speye-pattern.png')";
+      case 'des ombres au plafond':
+        return "url('/images/DesOmbresAuPlafond.jpg')";
+      case "le prix de l'attention":
+        return "url('/images/LePrixDeLAttention.jpg')";
+      case 'z.a. (wt)':
+        return "url('/images/za.jpg')";
+      case 'city scout':
+        return "url('/images/cityscout.jpg')";
+      case 'agora (wt)':
+        return "url('/images/agora.jpg')";
+      case 'parchemin (wt)':
+        return "url('/images/parchemin.png')";
       default:
         return 'none';
     }
@@ -28,7 +55,48 @@ export default function Projects() {
         return <FaYoutube className='text-red-700 text-3xl' />;
       case 'video-game':
         return <FaGamepad className='text-emerald-600 text-3xl' />;
+      default:
+        return null;
     }
+  };
+
+  const getProjectStatus = (status) => {
+    switch (status.toLowerCase()) {
+      case 'en cours':
+        return (
+          <span className='px-3 py-1 w-28 bg-blue-500 text-white text-sm flex flex-row items-center justify-center rounded'>
+            <FaPlayCircle className='mr-2' /> En cours
+          </span>
+        );
+      case 'en attente':
+        return (
+          <span className='px-3 py-1 w-28 bg-orange-500 text-white text-sm flex flex-row items-center justify-center rounded'>
+            <FaPauseCircle className='mr-2' /> En attente
+          </span>
+        );
+      case 'en ligne':
+        return (
+          <span className='px-3 py-1 w-28 bg-green-500 text-white text-sm flex flex-row items-center justify-center rounded'>
+            <FaCheckCircle className='mr-2' /> En ligne
+          </span>
+        );
+      default:
+        return null;
+    }
+  };
+
+  const handleNextImage = () => {
+    setCurrentImageIndex(
+      (prevIndex) => (prevIndex + 1) % selectedProject.images.length
+    );
+  };
+
+  const handlePrevImage = () => {
+    setCurrentImageIndex(
+      (prevIndex) =>
+        (prevIndex - 1 + selectedProject.images.length) %
+        selectedProject.images.length
+    );
   };
 
   return (
@@ -50,7 +118,7 @@ export default function Projects() {
             key={project.id}
             className={`p-6 text-white bg-white dark:bg-gray-700 rounded drop-shadow-lg relative group overflow-hidden`}>
             <div
-              className='absolute inset-0 opacity-0 group-hover:opacity-20 transition-opacity duration-500'
+              className='absolute inset-0 opacity-0 group-hover:opacity-20 transition-opacity duration-1000'
               style={{
                 backgroundImage: getPatternByProjectName(project.name),
                 backgroundSize: 'cover',
@@ -63,8 +131,13 @@ export default function Projects() {
               <div className='mr-8'>{getIconByType(project.type)}</div>
               <div className='w-full'>
                 <h3 className='brand-ttf'>{project.name}</h3>
-                <p>{project.description}</p>
-                <button className='p-2 mt-2 text-white text-xs bg-gradient-to-tl from-gray-900 to-blue-900 flex flex-row items-center rounded'>
+                <p className='text-sm'>{project.description}</p>
+                <button
+                  onClick={() => {
+                    setSelectedProject(project);
+                    setCurrentImageIndex(0);
+                  }}
+                  className='p-2 mt-2 text-white text-xs bg-blue-900 flex flex-row items-center rounded cursor-pointer'>
                   <FaEye className='mr-2' /> Voir plus
                 </button>
               </div>
@@ -72,6 +145,70 @@ export default function Projects() {
           </motion.div>
         ))}
       </div>
+
+      {/* MODAL */}
+      <AnimatePresence>
+        {selectedProject && (
+          <motion.div
+            className='fixed inset-0 bg-black bg-opacity-70 flex items-center justify-center z-50'
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}>
+            <motion.div
+              className='text-white bg-white dark:bg-gray-800 rounded-lg p-6 max-w-4xl w-full relative'
+              initial={{ scale: 0.8 }}
+              animate={{ scale: 1 }}
+              exit={{ scale: 0.8 }}
+              transition={{ duration: 0.3 }}>
+              <button
+                onClick={() => setSelectedProject(null)}
+                className='absolute top-4 right-4 text-white bg-red-600 rounded-full p-2 hover:bg-red-800 transition'>
+                <FaTimes />
+              </button>
+
+              <h3 className='text-2xl mb-4 brand-ttf'>
+                {selectedProject.name}
+              </h3>
+              <h2 className='my-2 text-'>
+                {getProjectStatus(selectedProject.status)}
+              </h2>
+              <p className='mb-4 text-sm'>{selectedProject.fullDescription}</p>
+
+              {/* Carrousel */}
+              {selectedProject.images && selectedProject.images.length > 0 && (
+                <div className='relative'>
+                  <img
+                    src={selectedProject.images[currentImageIndex]}
+                    alt={`${selectedProject.name} image`}
+                    className='rounded-lg w-full h-96 object-cover'
+                  />
+                  <button
+                    onClick={handlePrevImage}
+                    className='absolute left-0 w-auto top-1/2 transform -translate-y-1/2 bg-gray-700 text-white p-2 rounded-full hover:bg-gray-900'>
+                    <FaArrowLeft />
+                  </button>
+                  <button
+                    onClick={handleNextImage}
+                    className='absolute right-0 w-auto top-1/2 transform -translate-y-1/2 bg-gray-700 text-white p-2 rounded-full hover:bg-gray-900'>
+                    <FaArrowRight />
+                  </button>
+                </div>
+              )}
+
+              {/* Badges */}
+              <div className='mt-4 flex flex-wrap gap-2'>
+                {selectedProject.technologies?.map((tech, index) => (
+                  <span
+                    key={index}
+                    className='px-3 py-1 bg-blue-500 text-white text-sm rounded-full'>
+                    {tech}
+                  </span>
+                ))}
+              </div>
+            </motion.div>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </section>
   );
 }
